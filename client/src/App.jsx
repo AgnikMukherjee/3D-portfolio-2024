8b011190ed2c87ca637b171f187d209cb214bd34
AgnikMukherjee <agnikmukherjee78@gmail.com>
2025-07-22 20:53:33 +0530

    About section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import About from './components/About'
 
 const App = () => {
   return (
     <div className='container mx-auto max-w-7xl'>
       <Navbar/>
       <Hero/>
+      <About/>
     </div>
   )
 }
